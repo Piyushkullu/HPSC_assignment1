b65278c1e81bda4b31d34f8962b6a55bd30acf72
Piyush <158177449+Piyushkullu@users.noreply.github.com>
2026-04-04 11:38:29 +0000

    removed constants from record keeping

--- parallel/constants.f90
+++ parallel/constants.f90
@@ -3,12 +3,12 @@ module constants
     implicit none
     integer, parameter :: wp = 4
     real(kind=wp), parameter :: dt = 0.0001           ! timestep
-    real(kind=wp), parameter :: simulation_time = 2
+    real(kind=wp), parameter :: simulation_time = 1
     integer, parameter :: no_of_frames_to_skip = 300
-    integer, parameter :: N = 1  ! number of particles
+    integer, parameter :: N = 500  ! number of particles
     real(kind=wp), parameter :: L_x = 10,L_y = 10,L_z = 10 ! the dimensions of the box
     real (kind = wp), dimension(3) :: L = (/L_x,L_y,L_z/)
-    real(kind=wp), parameter :: max_velocity = 0
+    real(kind=wp), parameter :: max_velocity = 2
     real(kind=wp), parameter :: mass = 0.1
     real(kind=wp), parameter :: g = 9.81
     real(kind=wp), parameter :: radius = 0.1!radius of the interacting particles
