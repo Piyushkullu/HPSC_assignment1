d908199acd6c9468e69616f86c86fed8f82c2b3a
Piyush <158177449+Piyushkullu@users.noreply.github.com>
2026-04-04 11:10:24 +0000

    changed updation rules to be more stable

--- parallel/updater_function.f90
+++ parallel/updater_function.f90
@@ -1,25 +1,28 @@
 !Updater function definition ####
-
 subroutine updater_function(pos,vel,force)
     use constants
     implicit none
     real(kind = wp), dimension(3,N), intent(inout) :: pos, vel, force
-    pos = pos + vel*dt
-    vel = vel + (force/mass)*dt
+    vel = vel + (force/mass)*dt ! for semi - implicit vel must be updated first
+    pos = pos + vel*dt ! pos updated using the updated velocity
     !### updating force ###
     call calc_force(pos,vel,force)
 end subroutine updater_function
+
+
+!! For parallelising the pos and velocity updation negligible difference.
+
 ! subroutine updater_function(pos,vel,force)
+!     use omp_lib
 !     use constants
 !     implicit none
 !     real(kind = wp), dimension(3,N), intent(inout) :: pos, vel, force
 !     integer :: i
-!     !$omp do
+!     !$omp parallel do private(i)
 !     do i = 1, N
-!         pos(:, i) = pos(:, i) + vel(:, i) * dt
 !         vel(:, i) = vel(:, i) + (force(:, i) / mass) * dt
+!         pos(:, i) = pos(:, i) + vel(:, i) * dt
 !     end do
-!     !$omp end do 
+!     !$omp end parallel do 
 !     call calc_force(pos,vel,force)
-
 ! end subroutine updater_function
